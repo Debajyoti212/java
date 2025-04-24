5ae4a7d363e00d4f9162b084bd8f86584de4511a
Debajyoti Majumder <debajyoti344@gmail.com>
2025-04-24 16:06:32 +0530

    Add files via upload

---
+++ relationaloperator6.java
@@ -0,0 +1,10 @@
+public class relationaloperator6 {
+    public static void main(String[]args){
+        int a=12;
+        int b=13;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+}
