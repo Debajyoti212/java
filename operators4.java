11b2ab2da3f60c8e43c2f7fbe9ed305df5b1f9b1
Debajyoti Majumder <debajyoti344@gmail.com>
2025-04-24 15:08:24 +0530

    Add files via upload

--- operators4.java
+++ operators4.java
@@ -4,5 +4,16 @@ public class operators4 {
         int b=7;
         int result=a+b;
         System.out.println(result);
+      
+        int result1=a-b;
+        System.out.println(result1);
+        
+        int result2=a*b;
+        System.out.println(result2);
+        
+        float result3=(float)a/b;
+        System.out.println(result3);
+        float result4=a%b;
+        System.out.println(result4);
     }
 }
