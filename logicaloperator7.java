7104462732fed96c19512277cbc5f9c99279da89
Debajyoti Majumder <debajyoti344@gmail.com>
2025-04-24 16:06:54 +0530

    Add files via upload

---
+++ logicaloperator7.java
@@ -0,0 +1,10 @@
+public class logicaloperator7 {
+    public static void main(String[] args) {
+       boolean firstExpression =false;
+       boolean secondExpression=true;
+       System.out.println(firstExpression&&secondExpression); 
+       System.out.println(firstExpression||secondExpression); 
+       System.out.println(!firstExpression); 
+    }
+    
+}
