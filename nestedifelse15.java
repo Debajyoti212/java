e820ad55d1c187dea25df8edafd76c7f8c6e03bd
Debajyoti Majumder <debajyoti344@gmail.com>
2025-04-26 00:21:12 +0530

    Add files via upload

--- nestedifelse15.java
+++ nestedifelse15.java
@@ -2,25 +2,36 @@ import java.util.Scanner;
 
 public class nestedifelse15 {
     public static void main(String[] args) {
-       
-                int a = 9;
-                int b = 8;
-                int c = 1;
-        
-                if (a > b) {
-                    System.out.println("a is bigger than b");
-                    if (a > c) {
-                        System.out.println("a is bigger than c");
-                        System.out.println("a is largest " + a);
-                    } else {
-                        System.out.println("c is bigger than a");
-                        System.out.println("c is largest " + c);
-                    }
-                } else {
-                    // code for b >= a can go here if needed
-                }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the age of a ");
+        int a = sc.nextInt();
+        System.out.println("the age is " + a);
+
+        System.out.println("enter the age of b ");
+        int b = sc.nextInt();
+        System.out.println("the age is " + b);
+
+        System.out.println("enter the age of a ");
+        int c = sc.nextInt();
+        System.out.println("the age is " + c);
+        if (a > b) {
+            System.out.println("a is bigger than b");
+            if (a > c) {
+                System.out.println("a is bigger than c");
+                System.out.println("a is the largest" + a);
+            } else {
+                System.out.println("c is greatest");
+                System.out.println("c is largest" + c);
+            }
+        } else {
+            System.out.println("b is bigger than a");
+            if (b > c) {
+                System.out.println("b is greater than c");
+                System.out.println("b is largest" + c);
+            } else {
+                System.out.println("c is bigger than b");
+                System.out.println("c is largest" + c);
             }
         }
-        
-    
-
+    }
+}
