b3ea8a20f849884cf4b6bfd73aa2b1ed68883be8
Debajyoti Majumder <debajyoti344@gmail.com>
2025-05-12 01:13:47 +0530

    Add files via upload

---
+++ foreachloop25.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class foreachloop25 {
+    public static void main(String[] args) {
+        String names[] = { "dev", "shyam", "karan" };
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name is " + names[i]);
+        }
+    }
+
+}
