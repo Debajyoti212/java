da23d2164009b3e873f7b38270d092e323e7c8ce
Debajyoti Majumder <debajyoti344@gmail.com>
2025-05-12 16:07:35 +0530

    Add files via upload

---
+++ multdmnarray29.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class multdmnarray29 {
+    public static void main(String[] args) {
+        int marks[][] = {
+                { 12, 20, 34 },
+                { 33, 45, 13 },
+                { 90, 56, 24 }
+        };
+        System.out.println(Arrays.deepToString(marks));
+    }
+
+}
