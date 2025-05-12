1ff3024073d6060a58114d2b748970d3edc03caf
Debajyoti Majumder <debajyoti344@gmail.com>
2025-05-13 00:49:33 +0530

    Add files via upload

---
+++ method30.java
@@ -0,0 +1,13 @@
+public class method30 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        greet();
+        System.out.println(5);
+        
+    }
+    static void greet(){
+System.out.println(3);
+System.out.println(4);
+    }
+}
